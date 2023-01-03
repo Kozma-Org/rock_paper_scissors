afa197e25f28d0e7bccfdde829ebd4a01c2e5492
Szilveszter Kozma <skozma@diligent.com>
2023-01-03 17:27:18 +0100

    Complete the App layout

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   }
 
   root 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :games, only: [] do
+    get :play, on: :collection
+  end
 end
